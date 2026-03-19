d3c8a1e013f18ec4f1ab31cb2f6ec32b34fe78f6
ArturErnandes <al211205@yandex.ru>
2026-03-19 23:12:51 +0300

    точка входа

--- python/main.py
+++ python/main.py
@@ -1,10 +1,15 @@
 import cv2
 
-from config import camera
+from camera_classes import ColorRecognizer, ObjectDetector, Visualizer
+from config import camera, colors
 from logger import get_logger
 
 
 logger = get_logger(__name__)
+
+color_recognizer = ColorRecognizer(colors)
+object_detector = ObjectDetector()
+visualizer = Visualizer()
 
 capture = cv2.VideoCapture(camera)
 
@@ -14,7 +19,14 @@ while True:
         logger.error("Failed to capture frame")
         break
 
-    proccessed_frame =
+    masks = color_recognizer.create_masks(frame)
+    detections = object_detector.detect(masks)
+    processed_frame = visualizer.draw(frame, detections)
+
+    cv2.imshow("frames", processed_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
 capture.release()
 cv2.destroyAllWindows()
