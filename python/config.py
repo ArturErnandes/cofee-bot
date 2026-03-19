4d6a21d9fa2067226997015a1205a6c47bbd1e68
ArturErnandes <al211205@yandex.ru>
2026-03-19 23:12:40 +0300

    загрузка цветов

--- python/config.py
+++ python/config.py
@@ -1,17 +1,36 @@
 import numpy as np
+from camera_classes import Color
 
 
-camera = 0
+def fill_colors(colors_dict):
+    colors_list = []
+    for color in colors_dict:
+        colors_list.append(
+            Color(
+                name=color["name"],
+                lower_value=color["lower"],
+                upper_value=color["upper"],
+            )
+        )
+
+    return colors_list
+
+
+camera = '../../videos/coridor.MOV'
 
 color_ranges = [
     {
-        "label": "front-section",
+        #red
+        "title": "front-section",
         "lower": np.array([145, 120, 120], dtype=np.uint8),
         "upper": np.array([175, 255, 255], dtype=np.uint8),
     },
     {
-        "label": "back-section",
+        #blue
+        "title": "back-section",
         "lower": np.array([95, 120, 120], dtype=np.uint8),
         "upper": np.array([115, 255, 255], dtype=np.uint8),
     },
 ]
+
+colors = fill_colors(color_ranges)
