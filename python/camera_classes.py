c59fccb619d04fc9fba36396a274deec53f4d014
ArturErnandes <al211205@yandex.ru>
2026-03-20 13:00:08 +0300

    Класс DetectedObject

--- python/camera_classes.py
+++ python/camera_classes.py
@@ -17,7 +17,17 @@ class ColorMask:
     mask: NDArray[np.uint8]
 
 
-class ColorRecognizer:
+@dataclass
+class DetectedObject:
+    name: str
+    x_coord: int
+    y_coord: int
+    width: int
+    height: int
+    area: float
+
+
+class ColorsRecognizer:
     def __init__(self, colors: list[Color]):
         self.colors = colors
 
@@ -43,6 +53,43 @@ class ColorRecognizer:
                 )
             )
         return masks
+
+
+class ObjectsRecognizer:
+    def __init__(self, kernel, min_area):
+        self.kernel = kernel
+        self.min_area = min_area
+
+    def filter_mask(self, color_mask: ColorMask):
+        filtered_mask = cv2.erode(color_mask.mask, self.kernel, iterations=1)
+        filtered_mask = cv2.dilate(filtered_mask, self.kernel, iterations=1)
+        return ColorMask(
+            name=color_mask.name,
+            mask=filtered_mask,
+        )
+
+    def detect_objects(self, color_mask: ColorMask):
+        detected_objects = []
+        contours, _ = cv2.findContours(color_mask.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            area = cv2.contourArea(contour)
+
+            if area < self.min_area:
+                continue
+
+            x_coord, y_coord, width, height = cv2.boundingRect(contour)
+            detected_object = DetectedObject(
+                name=color_mask.name,
+                x_coord=x_coord,
+                y_coord=y_coord,
+                width=width,
+                height=height,
+                area=area,
+            )
+            detected_objects.append(detected_object)
+        return detected_objects
+
 
 
 '''
