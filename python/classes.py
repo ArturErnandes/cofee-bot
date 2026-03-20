a0b353c9a5fe310c5eabc73261db4574ae0613de
ArturErnandes <al211205@yandex.ru>
2026-03-20 19:35:33 +0300

    Вспомогательные классы

---
+++ python/classes.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from numpy.typing import NDArray
+import numpy as np
+
+
+@dataclass
+class Color:
+    name: str
+    lower_value: NDArray[np.uint8]
+    upper_value: NDArray[np.uint8]
+
+
+@dataclass
+class ColorMask:
+    name: str
+    mask: NDArray[np.uint8]
+
+
+@dataclass
+class DetectedObject:
+    name: str
+    x_coord: int
+    y_coord: int
+    width: int
+    height: int
+    area: float
+
+
+@dataclass
+class ObjectsConfig:
+    kernel: NDArray[np.uint8]
+    min_area: int
+    filter_iterations: int
+
+
+@dataclass
+class VisualizerConfig:
+    text_color: tuple[int, int, int]
+    border_color: tuple[int, int, int]
+    thickness: int
