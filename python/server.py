b67f9dbaf8e3a32fa228e25c6174391e1224ad03
ArturErnandes <al211205@yandex.ru>
2026-04-29 12:52:52 +0300

    docs/images

---
+++ python/server.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+from classes import Command
+
+
+app = FastAPI()
+
+
+@app.post("/command", tags=["Команды"], summary="Передача новой команды серверу")
+def put_command(command: str, time: float):
+    command = Command(
+        message=command,
+        time=time,
+    )
+    return {"message": f"Command '{command.message}' executed successfully"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="localhost", port=8080)
